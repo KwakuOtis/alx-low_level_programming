2abaad27cd63f34d961bf71a7dea0cb81485b5d3
Richmond Asare <takyea@yahoo.com>
2023-06-15 21:05:25 -0700

    First C code using betty

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -Enty of my first C line to print with function
+ * Return:0(Success)
+ */
+int main(void)
+{
+put("\"Programming is like building a multilingual puzzle");
+return(0);
+}
+	
