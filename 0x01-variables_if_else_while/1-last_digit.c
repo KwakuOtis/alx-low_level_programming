a3566bcf5c372dbb2a481f1ee52ec65e5b9a5e8b
Richmond Asare <takyea@yahoo.com>
2023-06-19 13:37:38 -0700

    Defining numbers

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,8 +1,12 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
 /**
- * main- Print last number of selctive random number
- * ad whether it less than 6 or greater than5 or 0
+ * main - Prints the last digit of a randomly generated number
+ *        and whether it is greater than 5, less than 6, or 0.
  *
- * Return Always 0.
+ * Return: Always 0.
  */
 int main(void)
 {
@@ -14,19 +18,19 @@ int main(void)
 	if ((n % 10) > 5)
 	{
 		printf("Last digit of %d is %d and is greater than 5\n",
-				n, n % 10);
+			n, n % 10);
 	}
 	else if ((n % 10) < 6 && (n % 10) != 0)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n",
-				n, n % 10);
+			n, n % 10);
 	}
-	else 
+	else
 	{
 		printf("Last digit of %d is %d and is 0\n",
-				n, n % 10);
+			n, n % 10);
 	}
+
 	return (0);
+
 }
-
-
