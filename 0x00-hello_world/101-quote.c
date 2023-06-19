b632313182eea6a1b9173c107e74a9dfef4fe2d3
Richmond Asare <takyea@yahoo.com>
2023-06-19 13:19:08 -0700

    Print second command

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - C program that prints a line for a standard erro
- * Return 1 (Success)
- */
+* main - Print a line for a second error
+*
+* Return: Always 1 (Success)
+*/
 int main(void)
 {
-	fput("and that piece of art is useful\"- Dora Korpar, 2015-10-19\n" stdout);
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
